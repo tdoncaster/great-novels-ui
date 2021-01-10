0af99c22288c672981606226b22cba5da806b902
unknown <tim doncaster>
2021-01-09 22:12:52 -0500

    updated LF

--- controllers/genres.js
+++ controllers/genres.js
@@ -1,25 +1,23 @@
-const models = require('../models')
+import models from '../models'
 
-const getAllGenres = async (request, response) => {
+export const getAllGenres = async (request, response) => {
   const genres = await models.Genres.findAll()
 
   return response.send(genres)
 }
 
-const getGenreById = async (request, response) => {
+export const getGenreById = async (request, response) => {
   const { id } = request.params
 
   const genre = await models.Genres.findOne({
     where: { id },
     include: [{
       model: models.Novels,
-      include: [{ model: models.Authors }]
-    }]
+      include: [{ model: models.Authors }],
+    }],
   })
 
   return genre
     ? response.send(genre)
     : response.sendStatus(404)
 }
-
-module.exports = { getAllGenres, getGenreById }
